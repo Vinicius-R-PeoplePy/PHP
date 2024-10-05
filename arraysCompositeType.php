8a2e2be0e342f745b8e1597c02bcbe74447cd5e0
Vinicius-R-PeoplePy <vinicius.people.py@gmail.com>
2024-10-05 16:28:09 -0300

    PHP learning

---
+++ arraysCompositeType.php
@@ -0,0 +1,9 @@
+// create an indexed array of five colors and print each color using a loop 
+
+<?php 
+$colors = ["red", "green", "blue", "yellow", "purple"];
+
+foreach ($colors as $color) {
+    echo $color . "\n";
+}
+?>
---
+++ variableDeclaration.php
@@ -0,0 +1,10 @@
+<?php
+$intVar = 42; 
+$floatVar = 3.14;  
+$stringVar = "PHP is fun";
+$boolVar = true; 
+
+echo $intVar . "\n";
+echo $floatVar . "\n";
+echo $stringVar . "\n";
+echo $boolVar ? 'True' : 'False';
